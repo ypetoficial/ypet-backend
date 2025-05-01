66425f15f65c2efd9af8f629df36510cd063e30c
felipegomesoliveira <felipegomesboos@gmail.com>
2025-05-01 15:44:55 -0300

    feat: :building_construction: Add initial domains

---
+++ app/Models/Occurrence.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Occurrence extends Model
+{
+    /** @use HasFactory<\Database\Factories\OccurrenceFactory> */
+    use HasFactory;
+}
