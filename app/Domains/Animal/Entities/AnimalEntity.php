71196834f9bd35f20ffc56974e31f94ca98f43b3
marcusslv <marcusjapaslv@hotmail.com>
2025-07-23 09:31:28 -0300

    :construction: Melhorias no fillable

--- app/Domains/Animal/Entities/AnimalEntity.php
+++ app/Domains/Animal/Entities/AnimalEntity.php
@@ -3,8 +3,54 @@
 namespace App\Domains\Animal\Entities;
 
 use App\Models\Animal;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AnimalEntity extends Animal
 {
     protected $table = 'animals';
+
+    protected $fillable = [
+        'hash',
+        'tenant_id',
+        'company_id',
+        'tutor_id',
+        'name',
+        'type',
+        'gender',
+        'weight',
+        'birth_date',
+        'castrated',
+        'castration_at',
+        'castration_site',
+        'size',
+        'color',
+        'coat',
+        'characteristics',
+        'surname',
+        'entry_date',
+        'profile_picture',
+        'collection_site',
+        'collection_reason',
+        'microchip_number',
+        'registration_number',
+    ];
+
+    protected $casts = [
+        'weight' => 'integer',
+        'birth_date' => 'date',
+        'castrated' => 'boolean',
+        'entry_date' => 'date',
+        'castration_at' => 'date',
+    ];
+
+    public function tutor(): BelongsTo
+    {
+        return $this->belongsTo('App\Models\User', 'tutor_id');
+    }
+
+    public function historyAnimalStatus(): HasMany
+    {
+        return $this->hasMany(AnimalStatusEntity::class, 'animal_id', 'id');
+    }
 }
--- app/Domains/Animal/Entities/AnimalStatusEntity.php
+++ app/Domains/Animal/Entities/AnimalStatusEntity.php
@@ -3,8 +3,23 @@
 namespace App\Domains\Animal\Entities;
 
 use App\Models\Animal;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AnimalStatusEntity extends Animal
 {
     protected $table = 'animal_statuses';
+
+    protected $fillable = [
+        'status',
+        'description',
+        'animal_id',
+        'created_by',
+        'tenant_id',
+        'company_id',
+    ];
+
+    public function animal(): BelongsTo
+    {
+        return $this->belongsTo(Animal::class, 'animal_id');
+    }
 }
