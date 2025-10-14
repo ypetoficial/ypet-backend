a3f46924198db53d664371543c6b8d6e5d6cc3f6
Davi Bruno <sitespump@gmail.com>
2025-10-14 16:30:21 +0000

    Resolve conflitos e mantém configuração da DigitalOcean

--- config/filesystems.php
+++ config/filesystems.php
@@ -69,6 +69,19 @@ return [
             'bucket' => env('DO_SPACES_BUCKET'),
         ],
 
+	'spaces' => [
+	'driver' => 's3',
+    	'key' => env('DO_SPACES_KEY'),
+    	'secret' => env('DO_SPACES_SECRET'),
+    	'endpoint' => env('DO_SPACES_ENDPOINT'),
+    	'region' => env('DO_SPACES_REGION'),
+    	'bucket' => env('DO_SPACES_BUCKET'),
+	'url' => env('DO_SPACES_ENDPOINT') . '/' . env('DO_SPACES_BUCKET'),
+    	'visibility' => 'public',
+	],
+
+>>>>>>> Stashed changes
+
     ],
 
     /*
