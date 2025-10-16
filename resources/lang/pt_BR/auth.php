ad341d35f67207ef90981aade8e42e74ff9cc5cb
Davi Bruno <sitespump@gmail.com>
2025-10-16 18:03:07 +0000

    Corrige config/filesystems.php e remove duplicação de spaces

---
+++ resources/lang/pt_BR/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'failed'   => 'As credenciais fornecidas não correspondem aos nossos registros.',
+    'password' => 'A senha fornecida está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
+];
+
